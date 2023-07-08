7e3c0dc127a2c01f7bfd15a15ba798dcf914fd40
Alex Reginelli <x20232357@student.ncirl.ie>
2023-07-08 15:18:13 +0100

    homepage, css, js

---
+++ public_html/script.js
@@ -0,0 +1,23 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+function changeStart() {
+    var input = document.getElementById("startDate");
+    input.type = "date";
+    input.style.backgroundColor = "white";
+}
+
+function changeEnd() {
+    var input = document.getElementById("endDate");
+    input.type = "date";
+    input.style.backgroundColor = "white";
+}
+
+function sendDate() {
+    /*
+     * send data function
+     */
+}
+  
