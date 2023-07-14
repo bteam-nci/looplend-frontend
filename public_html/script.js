5beb3aa32d2401d3a313d2b18f0e81d7045bda8a
alexreginelli <108173894+alexreginelli@users.noreply.github.com>
2023-07-14 14:29:10 +0100

    preview conversation and chat, update css and js

--- public_html/script.js
+++ public_html/script.js
@@ -21,16 +21,43 @@ function sendDate() {
      */
 }
 
-/*
- * keep the footer down
- * 
- */
-$(window).scroll(function() {
-    if ($(this).scrollTop() < 100) {
-        $(".footer").hide();
-    }
-    else {
-        $(".footer").show();
-    }
-});
-  
+
+/**script for chat */
+const chatMessagesContainer = document.getElementById('chatMessages');
+        const messageInput = document.getElementById('messageInput');
+        const sendMessageButton = document.getElementById('sendMessageButton');
+
+        function addMessage(content, sender) {
+            const messageContainer = document.createElement('li');
+            messageContainer.classList.add('chat-message');
+            if (sender) {
+                messageContainer.classList.add('sender-message');
+            } else {
+                messageContainer.classList.add('receiver-message');
+            }
+            const messageContent = document.createElement('div');
+            messageContent.classList.add('message-content');
+            if (sender) {
+                messageContent.classList.add('sender-content');
+            } else {
+                messageContent.classList.add('receiver-content');
+            }
+
+            messageContent.textContent = content;
+
+            messageContainer.appendChild(messageContent);
+            chatMessagesContainer.appendChild(messageContainer);
+        }
+//automatic answer to check if the code is working, later will be changed for a real chat interaction
+        sendMessageButton.addEventListener('click', function () {
+            const message = messageInput.value.trim();
+
+            if (message !== '') {
+                addMessage(message, true);
+                setTimeout(function () {
+                    addMessage('Example of response', false);
+                }, 1000);
+
+                messageInput.value = '';
+            }
+        });
